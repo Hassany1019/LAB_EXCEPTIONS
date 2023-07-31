d3ad946f1f535173a25676a9e4e30c15acf231e5
Hassan Albukhaytan <hassan.s.albukhaytan@gmail.com>
2023-07-31 22:32:47 +0300

    Lab_10

---
+++ Lab_10.py
@@ -0,0 +1,14 @@
+
+def additoin(x, y):
+    x = 10
+    y = 20
+    try:
+        print("Addition:", x + b)
+    except Exception as e:
+        print("something went wrong" , e.__class__)
+    else:
+        print("the operation is successful")
+
+
+
+additoin(10, 20)
